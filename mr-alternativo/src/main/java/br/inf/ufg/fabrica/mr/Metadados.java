f60ce14e63d838761208d4303bd0ab7ff8d3f526
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2015-08-03 22:18:34 -0300

    documentacao

--- mr-alternativo/src/main/java/br/inf/ufg/fabrica/mr/Metadados.java
+++ mr-alternativo/src/main/java/br/inf/ufg/fabrica/mr/Metadados.java
@@ -1,7 +1,7 @@
 package br.inf.ufg.fabrica.mr;
 
 /**
- * Informações sobre um item de dados
+ * Informações sobre um item de toBytes
  * do Modelo de Referência.
  *
  * <p>Não inclui o dado propriamente dito.</p>
--- mr-alternativo/src/main/java/br/inf/ufg/fabrica/mr/ModeloDeReferencia.java
+++ mr-alternativo/src/main/java/br/inf/ufg/fabrica/mr/ModeloDeReferencia.java
@@ -6,11 +6,16 @@ import java.util.Map;
  * Fábrica de objetos baseados no Modelo de Referência
  * do openEHR.
  *
+ * <p>Um objeto que implementa esta interface permite
+ * criar um grafo de objetos em conformidade com o
+ * Modelo de Referência do openEHR.</p>
+ *
  * <p>Um objeto baseado no Modelo de Referência do openEHR
  * é um objeto em conformidade com as especificações
  * desse Modelo de Referência. Tais especificações são
  * acompanhadas de uma implementação de referência em
- * Java, disponível em {@link https://github.com/openEHR/java-libs}.
+ * Java, disponível em
+ * <a href="https://github.com/openEHR/java-libs">aqui</a>.
  * </p>
  *
  * <p>Esta é uma implementação alternativa. Não é um <i>fork</i>,
@@ -35,14 +40,14 @@ import java.util.Map;
  * <p>Em decorrência das decisões acima duas orientações
  * são experimentadas: (a) não é criada uma classe para
  * cada conceito (conforme implmentação de referência) e
- * (b) dados são armazenados em um vetor de bytes que
+ * (b) toBytes são armazenados em um vetor de bytes que
  * serializa um grafo típico baseado na implementação
  * de referência.</p>
  */
 public interface ModeloDeReferencia {
 
     /**
-     * Detalha a organização de dados
+     * Detalha a organização de toBytes
      * baseados no Modelo de Referência do openEHR.
      *
      * <p>
@@ -51,8 +56,8 @@ public interface ModeloDeReferencia {
      * qual a informação ali registrada.
      * </p>
      *
-     * <p>Os dados
-     * correspondentes podem ser obtidos por {@code #dados}.
+     * <p>Os toBytes
+     * correspondentes podem ser obtidos por {@code #toBytes}.
      * @return Estrutura onde, para cada valor inteiro fornecido,
      * tem-se detalhes do item de informação do Modelo de
      * Referência em questão.</p>
@@ -70,9 +75,27 @@ public interface ModeloDeReferencia {
      *
      * <p>A estrutura desta sequência de bytes é
      * obtida por {@code #estrutura}.</p>
-     * @return
+     *
+     * <p>O retorno deste método, em geral, é persistido
+     * e, quando apropriado, recuperado por meio do
+     * método {@link #fromBytes(byte[])}.</p>
+     *
+     * @return Vetor de bytes contendo uma instância
+     * do Modelo de Referência (MR) devidamente serializada.
+     *
+     * @see #fromBytes(byte[])
      */
-    byte[] dados();
+    byte[] toBytes();
+
+    /**
+     * Realiza processo inverso à serialização, geralmente
+     * empregado para permitir busca sobre os toBytes em
+     * conformidade com o Modelo de Referência.
+     *
+     * @param bytes Vetor de bytes serializados por meio
+     *              do método {@link #toBytes()}.
+     */
+    void fromBytes(byte[] bytes);
 
     /**
      * Serializa informações baseadas no MR em um
@@ -104,19 +127,27 @@ public interface ModeloDeReferencia {
     /**
      * Adiciona um identificador ({@code DV_IDENTIFIER}).
      *
-     * @param issuer
-     * @param assigner
-     * @param id
-     * @param type
-     * @return
+     * @param issuer Entidade que emite identificação.
+     * @param assigner Entidade que assina identificação.
+     * @param id Identificador propriamente dito.
+     * @param type Tipo da identificação.
+     *
+     * @return O identificador único deste identificador
+     * na estrutura.
      */
-    int adicionaDvIdentifier(String issuer, String assigner, String id, String type);
+    int adicionaDvIdentifier(
+            String issuer,
+            String assigner,
+            String id,
+            String type);
 
     /**
      * Adiciona um {@link java.net.URI} ({@code DV_URI}).
      *
-     * @param uri
-     * @return
+     * @param uri Sequência de caracteres correspondentes
+     *            à {@link java.net.URI}.
+     *
+     * @return O identificador único desta URI na estrutura.
      */
     int adicionaDvUri(String uri);
 
@@ -124,34 +155,41 @@ public interface ModeloDeReferencia {
      * Adiciona um {@link java.net.URI} cujo esquema é
      * "ehr" ({@code DvEHRURI}).
      *
-     * @param uri
-     * @return
+     * @param uri Sequência de caracteres correspondentes
+     *            à {@link java.net.URI}.
+     * @return O identificador único desta DvEHRURI na estrutura.
      */
     int adicionaDvEhrUri(String uri);
 
     /**
-     * Adiciona um identificador de terminologia ({@code TERMINOLOGY_ID}).
+     * Adiciona um identificador de terminologia
+     * ({@code TERMINOLOGY_ID}).
      *
-     * @param nome
-     * @param versao
-     * @return
+     * @param nome Nome da terminologia.
+     * @param versao Versão da terminologia.
+     * @return O identificador único do identificador de
+     * terminologia na estrutura.
      */
     int adicionaTerminologyId(String nome, String versao);
 
     /**
-     * Adiciona um identificador de terminologia ({@code TERMINOLOGY_ID}).
+     * Adiciona um identificador de terminologia
+     * ({@code TERMINOLOGY_ID}).
      *
-     * @param valor
-     * @return
+     * @param valor Sequência de caracteres que é uma
+     *              serialização de um identificador de
+     *              terminologia ({TERMINOLOGY_ID}).
+     * @return O identificador único deste identificador de
+     * terminologia na estrutura.
      */
     int adicionaTerminologyId(String valor);
 
     /**
      * Adiciona um código ({@code CODE_PHRASE}).
      *
-     * @param terminologyId
-     * @param codeString
-     * @return
+     * @param terminologyId Um identificador de terminologia.
+     * @param codeString A sequência correspondente ao código.
+     * @return O identificador único do código na estrutura.
      */
     int adicionaCodePhrase(String terminologyId, String codeString);
 
@@ -159,9 +197,10 @@ public interface ModeloDeReferencia {
      * Adiciona dado encapsulado em uma sequência de caracteres
      * ({@code DV_PARSABLE}).
      *
-     * @param valor
-     * @param formalismo
-     * @return
+     * @param valor Dado encapsulado propriamente dito.
+     * @param formalismo Formalismo empregado pelo encapsulamento.
+     * @return O identificador único do dado encapsulado na
+     * estrutura.
      */
     int adicionaDvParsable(String valor, String formalismo);
 
@@ -169,11 +208,15 @@ public interface ModeloDeReferencia {
      * Adiciona dado encapsulado em uma sequência de caracteres
      * ({@code DV_PARSABLE}).
      *
-     * @param codePhraseCharSet
-     * @param codePhraseLanguage
-     * @param valor
-     * @param formalismo
-     * @return
+     * @param codePhraseCharSet A codificação empregada pelo
+     *                          dado encapsulado.
+     * @param codePhraseLanguage A linguagem empregada pelo
+     *                           dado encapsulado.
+     * @param valor O dado encapsulado propriamente dito.
+     * @param formalismo O formalismo empregado pelo dado
+     *                   encapsulado.
+     * @return O identificador único do dado encapsulado na
+     * estrutura.
      */
     int adicionaDvParsable(
             String codePhraseCharSet,
@@ -182,19 +225,30 @@ public interface ModeloDeReferencia {
             String formalismo);
 
     /**
-     * Adiciona dados encapsulados em uma sequência de bytes ({@code DV_MULTIMEDIA}).
+     * Adiciona dado codificado
+     * ({@code DV_MULTIMEDIA}).
      *
-     * @param codePhraseCharSet
-     * @param codePhraseLinguagem
-     * @param textoAlternativo
-     * @param codePhraseTipoMidia
-     * @param codePhraseAlgoritmoCompressao
-     * @param integridade
-     * @param codePhraseAlgoritmoIntegridade
-     * @param hDvMultimediaThumbnail
-     * @param dvUri
-     * @param dados
-     * @return
+     * @param codePhraseCharSet A codificação empregada.
+     * @param codePhraseLinguagem A linguagem empregada.
+     * @param textoAlternativo Texto alternativo para os dados.
+     * @param codePhraseTipoMidia A codificação do tipo de mídia.
+     * @param codePhraseAlgoritmoCompressao O algoritmo de
+     *                                      compressão empregado.
+     * @param integridade A sequência de bytes que serve para
+     *                    verificar a integridade dos dados.
+     * @param codePhraseAlgoritmoIntegridade O algoritmo de
+     *                                       verificação de
+     *                                       integridade dos
+     *                                       dados.
+     * @param hDvMultimediaThumbnail O identificador único de
+     *                               dados codificados que serve
+     *                               como representação
+     *                               comprimida do presente
+     *                               dado codificado.
+     * @param dvUri Sequência de caracteres que é a URI do
+     *              dado codificado.
+     * @param dado O dado codificado propriamente dito.
+     * @return O identificador únido do dado codificado.
      */
     int adicionaDvMultimedia(
             String codePhraseCharSet,
@@ -206,5 +260,5 @@ public interface ModeloDeReferencia {
             String codePhraseAlgoritmoIntegridade,
             int hDvMultimediaThumbnail,
             String dvUri,
-            byte[] dados);
+            byte[] dado);
 }
