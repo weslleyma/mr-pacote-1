f2cc2d67ed9430be71f5234cefbfe625f7d49552
weslleyma <eng@weslleyaraujo.com>
2015-08-25 16:15:53 -0300

    Classe de Testes

---
+++ mr-alternativo/src/test/java/br/inf/ufg/fabrica/mr/IModeloDeReferenciaTest.java
@@ -0,0 +1,15 @@
+package br.inf.ufg.fabrica.mr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Classe para testes da classe IModeloDeReferencia.java
+ */
+
+public class IModeloDeReferenciaTest {
+
+
+
+}
